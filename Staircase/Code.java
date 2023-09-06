fd94a7dc4c9fd42aa5f4c4feb1d2672e838d6ba8
TackyLeek <wilsonsecord@gmail.com>
2023-09-06 13:00:11 -0400

    Update Code.java

--- Staircase/Code.java
+++ Staircase/Code.java
@@ -13,22 +13,25 @@ import static java.util.stream.Collectors.toList;
 class Result {
 
     /*
-     * Complete the 'diagonalDifference' function below.
+     * Complete the 'staircase' function below.
      *
-     * The function is expected to return an INTEGER.
-     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     * The function accepts INTEGER n as parameter.
      */
 
-    public static int diagonalDifference(List<List<Integer>> arr) {
-        int s1 = 0;
-        int s2 = 0;
-        for (int i = 0; i < arr.size(); i++) {
-            s1 += arr.get(i).get(i);
+    public static void staircase(int n) {
+        for (int j = (n - 1); j > 0; j--) {
+            for (int i = 0; i < j; i++) {
+                System.out.print(" ");
+            }
+            for (int i = 0; i < (n - j); i++) {
+                System.out.print("#");
+            }
+            System.out.println();
         }
-        for (int i = 0; i < arr.size(); i++) {
-            s2 += arr.get(i).get((arr.size()-1)-i);
+        for (int i = 0; i < n; i++) {
+            System.out.print("#");
         }
-        return Math.abs(s1 - s2);
+
     }
 
 }
@@ -36,30 +39,11 @@ class Result {
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int n = Integer.parseInt(bufferedReader.readLine().trim());
 
-        List<List<Integer>> arr = new ArrayList<>();
-
-        IntStream.range(0, n).forEach(i -> {
-            try {
-                arr.add(
-                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                        .map(Integer::parseInt)
-                        .collect(toList())
-                );
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-        });
-
-        int result = Result.diagonalDifference(arr);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
+        Result.staircase(n);
 
         bufferedReader.close();
-        bufferedWriter.close();
     }
 }
