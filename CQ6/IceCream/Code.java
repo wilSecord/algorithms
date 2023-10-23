5c2837c813fdcb096b21548b0a6893e315a0af15
wilSecord <wilsonsecord@gmail.com>
2023-10-23 12:56:06 -0400

    CQ6

---
+++ CQ6/IceCream/Code.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'icecreamParlor' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER m
+     *  2. INTEGER_ARRAY arr
+     */
+
+// I cannot figure this out for the life of me I don't know why
+    public static List<Integer> icecreamParlor(int m, List<Integer> arr) {
+        for (int i = 0; i < arr.size(); i++) {
+            List<Integer> out = icecreamParlor(m - arr.get(i), arr);
+            int s = 0;
+            for (Integer j : out) {
+                s += j;
+            }
+            if (s == m) {
+                return out;
+            }
+        }
+        return arr;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int m = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                List<Integer> result = Result.icecreamParlor(m, arr);
+
+                bufferedWriter.write(
+                    result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                    + "\n"
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
---
+++ CQ6/PlusMinus/README.md
@@ -0,0 +1,2 @@
+time: O(n), it iterates through the original list only once
+space: Aux is constant as it only instantiates three variables for any given length of list. Otherwise, linear because of the input.
